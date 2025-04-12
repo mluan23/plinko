a8b890fdfb91d2891001d15e1f53545d0df43d0b
luanm <mll2071@rit.edu>
2025-04-12 19:27:05 -0400

    Add boundaries for board

--- board.py
+++ board.py
@@ -15,16 +15,30 @@ class Board():
         self.obstacle_sprites = pygame.sprite.Group()
         self.updated_coords = OBSTACLE_START
 
+        self.segmentA_2 = OBSTACLE_START
+
         self.make_obstacles()
 
     def make_obstacles(self):
         while self.curr_row_count <= self.final_row_count:
             for i in range(self.curr_row_count):
+                if self.curr_row_count == 3 and self.updated_coords[0] > OBSTACLE_START[0] + OBSTACLE_PAD:
+                    self.segmentB_1 = self.updated_coords
+                elif self.curr_row_count == self.final_row_count and i ==0:
+                    self.segmentA_1 = self.updated_coords
+                elif self.curr_row_count == self.final_row_count and i == self.final_row_count - 1:
+                    self.segmentB_2 = self.updated_coords
                 self.obstacles_list.append(self.spawn_obstacle(self.updated_coords, self.space))
                 self.updated_coords = (int(self.updated_coords[0] + OBSTACLE_PAD), self.updated_coords[1])
             self.updated_coords = (int(WIDTH - self.updated_coords[0] + (.5 * OBSTACLE_PAD)), int(self.updated_coords[1] + OBSTACLE_PAD))
             self.curr_row_count += 1
-        print(self.obstacles_list)
+        self.multi_x, self.multi_y = self.updated_coords[0] + OBSTACLE_PAD, self.updated_coords[1]
+
+        # make boundaries
+        self.spawn_segments(self.segmentA_1, self.segmentA_2, self.space)
+        self.spawn_segments(self.segmentB_1, self.segmentB_2, self.space)
+        self.spawn_segments((self.segmentA_2[0], 0), self.segmentA_2, self.space)
+        self.spawn_segments(self.segmentB_1, (self.segmentB_1[0], 0), self.space)
     def spawn_obstacle(self, position, space):
         body = pymunk.Body(body_type=pymunk.Body.STATIC)
         body.position = position
@@ -45,6 +59,11 @@ class Board():
             pos_x = int(obstacle.body.position.x)
             pos_y = int(obstacle.body.position.y)
             pygame.draw.circle(self.display_surface, OBSTACLE_COLOR, (pos_x,pos_y), OBSTACLE_RAD)
+
+    def spawn_segments(self, pointA, pointB, space):
+        segment_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+        segment_shape = pymunk.Segment(segment_body, pointA, pointB, 5)
+        self.space.add(segment_body, segment_shape)
 
     def update(self):
         self.draw_obstacle(self.obstacles_list)
